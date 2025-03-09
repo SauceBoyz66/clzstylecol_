96817e9c173009b66b91e45ea86db0ac1437bcf4
темная ночь <losadaartunduaganicolas@gmail.com>
2025-03-09 17:39:43 -0600

    Add files via upload

---
+++ ola.js
@@ -0,0 +1,5 @@
+function agregarAlCarrito(productoId) {
+    // Aquí puedes agregar la lógica para almacenar los productos en el carrito
+    console.log(`Producto ${productoId} agregado al carrito`);
+    // Por ejemplo, podrías guardar los productos en el localStorage o enviar una solicitud AJAX al servidor.
+}
